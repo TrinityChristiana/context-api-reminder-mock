57d2eca2f7d90133c5722c40b230b4d60ec10ad8
Trinity Christiana <trinitycterry@gmail.com>
2021-02-07 13:51:45 -0600

    Removed unused variable, moved delete finctionality

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,12 +1,16 @@
 import React, { useState, useContext, useEffect } from "react";
 import TodoContext from "../contexts/context";
-import { ADD_TODO, UPDATE_TODO } from "../contexts/constants";
+import { ADD_TODO, UPDATE_TODO, DELETE_TODO } from "../contexts/constants";
 import FormToggleButton from "./FormToggleButton";
 
 const TodoForm = ({ item = {}, setIsUpdating }) => {
   const [newTodo, setTodo] = useState("");
   const [showForm, setShowForm] = useState(false);
-  const { state, dispatch } = useContext(TodoContext);
+  const { dispatch } = useContext(TodoContext);
+
+  const handleDelete = () => {
+    dispatch({ type: DELETE_TODO, payload: item });
+  };
 
   useEffect(() => {
     if (item.id) {
@@ -31,25 +35,28 @@ const TodoForm = ({ item = {}, setIsUpdating }) => {
     setTodo("");
   };
   return (
-    <div
-      className={`${
-        item.id ? "todo-list-item-form" : "todo-form"
-      } input-group input-group-lg`}
-    >
+    <div className={`${item.id ? "todo-form" : "todo-form-new"} input-group`}>
       {showForm && (
-        <input
-          type="text"
-          className="form-control"
-          placeholder="New Todo"
-          aria-label="New Todo"
-          value={newTodo}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              addTodo();
-            }
-          }}
-          onChange={(e) => setTodo(e.target.value)}
-        />
+        <>
+          <textarea
+            type="text"
+            autoFocus
+            onBlur={addTodo}
+            placeholder="New Todo"
+            aria-label="New Todo"
+            value={newTodo}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                addTodo();
+              }
+            }}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <i
+            className="todo-list-item-delete fas fa-trash"
+            onClick={handleDelete}
+          ></i>
+        </>
       )}
 
       {!item.id && !showForm && <FormToggleButton setShowForm={setShowForm} />}
